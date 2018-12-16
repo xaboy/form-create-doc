0cd275552cbea6c13f1ace38f306939a4af32fdb
xaboy <xaboy2005@qq.com>
2018-12-16 11:38:54 +0800

    first

--- docs/.vuepress/public/static/index.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?931b182e4333e09676463bcc8248f71e";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
